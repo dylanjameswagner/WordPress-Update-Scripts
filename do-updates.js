ef4b0e415c4d889fbc99e7cc2faea762cc3c644f
Jesse Tolj <jesse.tolj@teamcolab.com>
2019-11-06 15:18:21 -0500

    remove scare quotes

--- do-updates.js
+++ do-updates.js
@@ -4,7 +4,7 @@ let landoOutput = spawnSync('lando', ['wp', 'plugin', 'update', '--all', '--dry-
 pluginData = JSON.parse(landoOutput.stdout);
 
 for (let plugin of pluginData) {
-    console.log(`"Updating plugin ${plugin.name} from ${plugin.version} to ${plugin.update_version}"`);
+    console.log(`Updating plugin ${plugin.name} from ${plugin.version} to ${plugin.update_version}`);
     spawnSync('lando', ['wp', 'plugin', 'update', plugin.name, '--format=json'], { encoding : 'utf8' });
     console.log("Adding git commit...");
     spawnSync('git', ['add', '-A']),
