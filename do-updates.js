e1e6384bf6628f9c36cef2dce037e255c1d9180d
Jesse Tolj <jesse.tolj@teamcolab.com>
2019-11-06 15:24:55 -0500

    handle plugin update failures

--- do-updates.js
+++ do-updates.js
@@ -5,8 +5,20 @@ pluginData = JSON.parse(landoOutput.stdout);
 
 for (let plugin of pluginData) {
     console.log(`Updating plugin ${plugin.name} from ${plugin.version} to ${plugin.update_version}`);
-    spawnSync('lando', ['wp', 'plugin', 'update', plugin.name, '--format=json'], { encoding : 'utf8' });
-    console.log("Adding git commit...");
-    spawnSync('git', ['add', '-A']),
-    spawnSync('git', ['commit', '-m', `Update plugin ${plugin.name} from ${plugin.version} to ${plugin.update_version}`]);
+    let output = spawnSync('lando', ['wp', 'plugin', 'update', plugin.name, '--format=json'], { encoding : 'utf8' });
+    let resultData;
+    try {
+        resultData = JSON.parse(output.stdout);
+    } catch (e) {
+        console.log(e);
+        continue;
+    }
+    if (resultData[0].status == 'Updated') {
+        console.log("Adding git commit...");
+        spawnSync('git', ['add', '-A']),
+        spawnSync('git', ['commit', '-m', `Update plugin ${plugin.name} from ${plugin.version} to ${plugin.update_version}`]);
+    } else {
+        console.log(`Error updating ${plugin.name} from ${plugin.version} to ${plugin.update_version}`);
+    }
+    
 }
